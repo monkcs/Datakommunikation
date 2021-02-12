f01a373cecb58350092f9367c25ad12464fabd06
monkcs <charlie.habolin@monkcs.se>
2021-02-12 16:14:44 +0100

    Implemented parsing of response

--- Laboration 1/Web server/request.hpp
+++ Laboration 1/Web server/request.hpp
@@ -1,44 +1,72 @@
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
 
-#include <filesystem>
-#include <iostream>
+#include "protocol.hpp"
+
+#include <algorithm>
 #include <ranges>
 #include <string>
 #include <vector>
 
-/*
-int main()
-{
-	try
-	{
-		const std::vector<Field> fields {Field {FieldName::Connection, "close"}};
-
-		const auto header = Header {StatusLine {Protocol {Version::HTTP1}, Status {200}}, fields};
-
-		std::cout << header;
-	}
-	catch (const std::runtime_error&)
-	{
-	}
-}
-*/
-
 class Request
 {
+  public:
+	Request(const std::string request) { parse(request); };
+
   protected:
-	void parse(const std::string& request)
+	void parse(std::string request)
 	{
-		for (const auto word : "GET / HTTP/1.1" | std::ranges::views::split("\n\r"))
+		request.erase(std::remove(request.begin(), request.end(), '\r'), request.end());
+
+		std::vector<std::string> properties {};
+
+		for (auto line : (request | std::ranges::views::split('\n')))
 		{
-			for (auto c : t)
+			for (auto word : (line | std::ranges::views::split(' ')))
 			{
-				std::cout << c;
+				std::string command {};
+				for (auto character : word)
+				{
+					command += character;
+				}
+				properties.emplace_back(command);
 			}
-			std::cout << "\n";
+
+			/* Break after first line */
+			break;
+		}
+
+		if (properties.at(0) == "GET")
+		{
+			method = Method::Get;
+		}
+		else
+		{
+			throw std::runtime_error {"Unsupported request method"};
+		}
+
+		if (properties.at(1) != "")
+		{
+			path = properties.at(2)
+		}
+		else
+		{
+			throw std::runtime_error {"Invalid file path"};
+		}
+
+		if (properties.at(2) == "HTTP/1.1")
+		{
+			protocol = Protocol {Version::HTTP1};
+		}
+		else
+		{
+			throw std::runtime_error {"Unsupported http version"};
 		}
 	};
 
-	//	Method method;
-	//	std::filesystem::path path;
-	//	Protocol protocol;
-	//	std::string host;
+	Method method;
+	std::string path;
+	Protocol protocol;
 };
+
+#endif
