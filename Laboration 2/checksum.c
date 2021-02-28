acb716fb96a1b9d42d33e961f2da037cb83716a7
monkcs <charlie.habolin@monkcs.se>
2021-02-28 21:29:47 +0100

    Implement checksum functions

--- Laboration 2/checksum.c
+++ Laboration 2/checksum.c
@@ -4,46 +4,24 @@
 
 #include <stdbool.h>
 
-bool certify(struct pkt* const package)
+void certify(struct pkt* const package)
 {
-	if (package->checksum == 0)
+	package->checksum = 0;
+
+	int checksum = 0;
+	for (size_t i = 0; i < sizeof(*package); i++)
 	{
-		int checksum = 0;
-
-		for (size_t i = 0; i < sizeof(package); i++)
-		{
-			checksum += ((const char* const)package)[i];
-		}
-
-		package->checksum = ~checksum;
-
-		return true;
+		checksum += ((char*)package)[i];
 	}
-	else
-	{
-		return false;
-	}
+
+	package->checksum = checksum;
 }
 
 bool verify(struct pkt* const package)
 {
 	const int calculated = package->checksum;
-	package->checksum = 0;
 
-	if (certify(package))
-	{
-		if (calculated == package->checksum)
-		{
-			package->checksum = 0;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	else
-	{
-		return false;
-	}
+	certify(package);
+
+	return calculated == package->checksum;
 }
--- Laboration 2/checksum.h
+++ Laboration 2/checksum.h
@@ -1,13 +1,13 @@
 #pragma once
 
+/* The checksum mathod is the same as the algorithm used in TCP.*/
+
 #include "Stud.h"
 
 #include <stdbool.h>
 
-/* Certify the packet with a checksum. The member .checksum needs to
-   be zero, otherwise the function will return false. */
-bool certify(struct pkt* const package);
+/* Certify the packet with a checksum */
+void certify(struct pkt* const package);
 
-/* Verify if the checksum is valid. The member .checksum will be set to
-   zero if a sucessfull verification is done. */
+/* Verify if the checksum is valid */
 bool verify(struct pkt* const package);
